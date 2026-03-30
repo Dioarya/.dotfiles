c9cd6542b169581a925daec988e21e85fe6845c6
Dioarya <dioaryaraditya@gmail.com>
2026-03-31 00:57:15 +0700

    autocommit dotfiles

--- .config/nvim/lua/custom/configs/snacks/modules/dashboard.lua
+++ .config/nvim/lua/custom/configs/snacks/modules/dashboard.lua
@@ -1,101 +1,35 @@
----@class snacks.dashboard.Config
----@field enabled? boolean
----@field sections snacks.dashboard.Section
----@field formats table<string, snacks.dashboard.Text|fun(item:snacks.dashboard.Item, ctx:snacks.dashboard.Format.ctx):snacks.dashboard.Text>
-local defaults = {
-  width = 60,
-  row = nil, -- dashboard position. nil for center
-  col = nil, -- dashboard position. nil for center
-  pane_gap = 4, -- empty columns between vertical panes
-  autokeys = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", -- autokey sequence
-  -- These settings are used by some built-in sections
-  preset = {
-    -- Defaults to a picker that supports `fzf-lua`, `telescope.nvim` and `mini.pick`
-    ---@type fun(cmd:string, opts:table)|nil
-    pick = nil,
-    -- Used by the `keys` section to show keymaps.
-    -- Set your custom keymaps here.
-    -- When using a function, the `items` argument are the default keymaps.
-    ---@type snacks.dashboard.Item[]
-    keys = {
-      { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
-      { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-      { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
-      { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
-      {
-        icon = " ",
-        key = "c",
-        desc = "Config",
-        action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
-      },
-      { icon = " ", key = "s", desc = "Restore Session", section = "session" },
-      { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
-      { icon = " ", key = "q", desc = "Quit", action = ":qa" },
-    },
-    -- Used by the `header` section
-    header = [[
-███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
-████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
-██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
-██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
-██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
-╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝]],
-  },
-  -- item field formatters
-  formats = {
-    icon = function(item)
-      if item.file and item.icon == "file" or item.icon == "directory" then
-        return Snacks.dashboard.icon(item.file, item.icon)
-      end
-      return { item.icon, width = 2, hl = "icon" }
-    end,
-    footer = { "%s", align = "center" },
-    header = { "%s", align = "center" },
-    file = function(item, ctx)
-      local fname = vim.fn.fnamemodify(item.file, ":~")
-      fname = ctx.width and #fname > ctx.width and vim.fn.pathshorten(fname) or fname
-      if #fname > ctx.width then
-        local dir = vim.fn.fnamemodify(fname, ":h")
-        local file = vim.fn.fnamemodify(fname, ":t")
-        if dir and file then
-          file = file:sub(-(ctx.width - #dir - 2))
-          fname = dir .. "/…" .. file
-        end
-      end
-      local dir, file = fname:match "^(.*)/(.+)$"
-      return dir and { { dir .. "/", hl = "dir" }, { file, hl = "file" } } or { { fname, hl = "file" } }
-    end,
-  },
-  sections = {
-    { section = "header" },
-    { section = "keys", gap = 1, padding = 1 },
-    { section = "startup" },
-  },
-}
+local width = 60
+local ok, env = pcall(require, "env")
 
-local width_image = 120
-local height_image = width_image / 4 -- chafa height is terminal rows, so height/2 for aspect ratio
-local width_keys = 60
-local indent_keys = width_image - width_keys
+local keys = { section = "keys", align = "center", gap = 1 }
+if ok then
+  keys = nil
+end
 
-local sections = {
-  gap = 1,
-  {
+local header = { section = "header" }
+if ok then
+  local width_image = 120
+  local height_image = math.floor(width_image / 16 * 9 / 2)
+  header = {
     section = "terminal",
-    cmd = require("env").dashboard_terminal_cmd(width_image, height_image),
-    indent = -indent_keys / 2,
-    width = width_image, -- only affects chafa render size
+    cmd = env.dashboard_terminal_cmd(width_image, height_image),
+    indent = -math.floor((width_image - width) / 2),
+    width = width_image,
     height = height_image,
-    random = 9999999, -- Do not cache
-  },
-  { section = "keys", align = "center", gap = 1 },
-  { section = "startup" },
-}
+    ttl = 0,
+  }
+end
 
-local opts = vim.deepcopy(defaults)
-opts["width"] = width_image - indent_keys
-opts["height"] = 20
-opts["sections"] = sections
-opts["pane_gap"] = 4
+local startup = { section = "startup" }
+
+local sections = { gap = 1 }
+table.insert(sections, header)
+table.insert(sections, keys)
+table.insert(sections, startup)
+
+local opts = {
+  width = width,
+  sections = sections,
+}
 
 return opts
---
+++ .config/nvim/lua/plugins/mason-nvim-lint.lua
@@ -0,0 +1,5 @@
+return {
+  "rshkarin/mason-nvim-lint",
+  dependencies = require "custom.configs.mason-nvim-lint.deps",
+  opts = require "custom.configs.mason-nvim-lint.opts",
+}
